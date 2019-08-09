9070447a472aeb42c794c143c9a9596ed48ecf2f
Enrico Secondulfo <enrico.secondulfo@fidesdigital.com>
2019-08-09 16:49:53 +0200

    release 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     val kotlinVersion = "1.3.41"
     val springDependencyManagementVersion = "1.0.7.RELEASE"
+    val bintrayVersion = "1.8.4"
 
     kotlin("jvm") version kotlinVersion
     id("io.spring.dependency-management") version springDependencyManagementVersion
     id("java-library")
-
+    id("com.jfrog.bintray") version bintrayVersion
+    `maven-publish`
 }
 
 group = "it.github.enricosecondulfo"
@@ -18,6 +21,7 @@ repositories {
     maven("https://repo.spring.io/milestone")
     maven("https://repo.spring.io/snapshot")
     maven("https://dl.bintray.com/hotkeytlt/maven")
+
     jcenter()
 }
 
@@ -30,6 +34,46 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+fun findProperty(property: String) = project.findProperty(property) as String?
+
+bintray {
+    user = findProperty("bintrayUser")
+    key = findProperty("bintrayApiKey")
+    publish = true
+    setPublications("mavenJava")
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = findProperty("artifactId")
+        websiteUrl = "https://github.com/enricosecondulfo/sberqoolt"
+        githubRepo = "enricosecondulfo/sberqoolt"
+        vcsUrl = "https://github.com/enricosecondulfo/sberqoolt"
+        description = "Rql parser for Spring Framework that convert rql string in Spring Data MongoDB TypedAggregation"
+        setLabels("kotlin")
+        setLicenses("Apache-2.0")
+        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+            name = findProperty("version")
+            vcsTag = findProperty("version")
+        })
+    })
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            groupId = findProperty("groupId")
+            artifactId = findProperty("artifactId")
+            version = findProperty("version")
+            artifact(sourcesJar.get())
+        }
+    }
 }
 
 dependencyManagement {
